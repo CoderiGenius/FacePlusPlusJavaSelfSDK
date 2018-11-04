3a76cfbe2442aa8c8b4e7e71f886c141c7d84feb
CoderiGenius <755717181@qq.com>
2018-11-04 19:21:50 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,14 +12,14 @@ public class Main {
 
        /* DetactFace detactFace = new DetactFace();
 
-        com.alibaba.fastjson.JSONObject jsonObject =  detactFace.Detact("D:\\1\\src\\test\\1810010311_王雅琪_2_青岛科技大学+崂山校区+外国语学院+英语(语言)+英语183_000000081003.jpg");
+        com.alibaba.fastjson.JSONObject jsonObject =  detactFace.Detact("");
         System.out.println(jsonObject.get("faces"));
         com.alibaba.fastjson.JSONArray jsonArray = JSON.parseArray(jsonObject.get("faces").toString());
 */
 
         /*SetFaceID setFaceID = new SetFaceID();
         System.out.println(setFaceID.Set("064850e6ddb3d9e227360c588b4f22a8"
-                ,getFileName("D:\\1\\src\\test\\1810010311_王雅琪_2_青岛科技大学+崂山校区+外国语学院+英语(语言)+英语183_000000081003.jpg")));*/
+                ,getFileName("")));*/
         FaceSet faceSetForAddFace = new FaceSet();
         System.out.println(faceSetForAddFace.AddFaceToFaceSet("18", "83fd2402890144690669c799ba16c858"));
     }
